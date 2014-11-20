32db4f7e7fbdbdcec743f4ffbf84db73d9bb7efa
Collin Eye <p_c_eye@yahoo.com>
2014-11-20 17:23:00 -0600

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to nature-of-code
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/nature_of_code/core_test.clj
@@ -0,0 +1,7 @@
+(ns nature-of-code.core-test
+  (:require [clojure.test :refer :all]
+            [nature-of-code.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
